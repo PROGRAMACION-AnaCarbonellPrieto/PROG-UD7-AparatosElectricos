4b22717ec4731f55a79706f6fc0c62a787f52c0c
Ana Carbonell Prieto <anacarpri@alu.edu.gva.es>
2024-04-16 13:01:31 +0200

    Correccion terminada

--- src/main/java/prog/ud7/tests/TestAparatoElectrico.java
+++ src/main/java/prog/ud7/tests/TestAparatoElectrico.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package prog.ud7.test;
+package prog.ud7.tests;
 
-import java.awt.Color;
 import prog.ud7.aparatoselectricos.AparatoElectrico;
 import prog.ud7.aparatoselectricos.Computador;
-import prog.ud7.aparatoselectricos.Electrodomestico;
+import prog.ud7.aparatoselectricos.Electrodomestico.Color;
+import prog.ud7.aparatoselectricos.Electrodomestico.ConsumoEnergetico;
 import prog.ud7.aparatoselectricos.Lavadora;
 import prog.ud7.aparatoselectricos.Movil;
 import prog.ud7.aparatoselectricos.Television;
@@ -21,10 +21,10 @@ public class TestAparatoElectrico {
     public static void main(String[] args) {
         AparatoElectrico[] aparatos = new AparatoElectrico[8];
         
-        aparatos[0] = new Lavadora(5, Electrodomestico.Color.BLANCO, Electrodomestico.ConsumoEnergetico.F, 100, "Bosch", "V1", 5, "1234A");
-        aparatos[1] = new Lavadora(12, Electrodomestico.Color.BLANCO, Electrodomestico.ConsumoEnergetico.F, 100, "Fagor", "F22", 5, "1234B");
-        aparatos[2] = new Television(45, Electrodomestico.Color.BLANCO, Electrodomestico.ConsumoEnergetico.F, 100, "LG", "SuperGuay", 5, "1234C");
-        aparatos[3] = new Television(14, Electrodomestico.Color.BLANCO, Electrodomestico.ConsumoEnergetico.F, 100, "Sony", "MásChulaA", 5, "1234D");
+        aparatos[0] = new Lavadora(5, Color.BLANCO, ConsumoEnergetico.F, 100, "Bosch", "V1", 5, "1234A");
+        aparatos[1] = new Lavadora(12, Color.BLANCO, ConsumoEnergetico.F, 100, "Fagor", "F22", 5, "1234B");
+        aparatos[2] = new Television(45, Color.BLANCO, ConsumoEnergetico.F, 100, "LG", "SuperGuay", 5, "1234C");
+        aparatos[3] = new Television(14, Color.BLANCO, ConsumoEnergetico.F, 100, "Sony", "MásChulaA", 5, "1234D");
         aparatos[4] = new Computador(2048, 5000, 1024, "1234E");
         aparatos[5] = new Computador(1024, 10000, 2048, "1234F");
         aparatos[6] = new Movil(Movil.Memoria.MB256, "Samsung", "Galaxy S21", "1234G");
